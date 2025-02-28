0f521f4fe24a18abf8dc26c0c7459605c753c2aa
Yuliya Bilous <yulya.al.belous@gmail.com>
2025-02-28 18:41:06 +0200

    added configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import {ContactListComponent} from "./features/contacts/contact-list/contact-list.component";
+import {CompanyListComponent} from "./features/companies/company-list/company-list.component";
+import {AddContactComponent} from "./features/contacts/add-contact/add-contact.component";
+import {AddCompanyComponent} from "./features/companies/add-company/add-company.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'contacts',
+    component: ContactListComponent
+  },
+  {
+    path: 'add-contact',
+    component: AddContactComponent
+  },
+  {
+    path: 'companies',
+    component: CompanyListComponent
+  },
+  {
+    path: 'add-company',
+    component: AddCompanyComponent
+  },
+  {
+    path: '',
+    redirectTo: '/contacts',
+    pathMatch: 'full'
+  }
+];
